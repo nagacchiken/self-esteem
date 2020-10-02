701c548ad6f7054920b05e8712825566f315e4fb
nagacchiken <vx458pl4@gmail.com>
2020-10-02 16:52:15 +0900

    ゲストログイン機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'life_charts#index'
-  resource only: [:index]
+  resource :life_charts, only: [:index]
+  post '/life_charts/guest_sign_in', to: 'life_charts#new_guest'
 end
