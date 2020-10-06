8cd167587945e704916f57849e565cd7c70cfb38
nagacchiken <vx458pl4@gmail.com>
2020-10-06 17:09:13 +0900

    FullCalendarの導入

---
+++ app/controllers/good_things_controller.rb
@@ -0,0 +1,4 @@
+class GoodThingsController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'good_things/index'
   devise_for :users
   root to: 'life_charts#index'
   resource :life_charts, only: [:index]
   post '/life_charts/guest_sign_in', to: 'life_charts#new_guest'
+  resource :good_things, only: [:index]
 end
