a161bf8970920342ffd27d2021eccaa04aad1a14
nagacchiken <vx458pl4@gmail.com>
2020-10-11 23:43:17 +0900

    投稿を編集できるよう実装

--- app/controllers/good_things_controller.rb
+++ app/controllers/good_things_controller.rb
@@ -9,16 +9,27 @@ class GoodThingsController < ApplicationController
     @good_thing = GoodThing.new(good_thing_params)
     
     if @good_thing.save
-    redirect_to root_path
+    redirect_to good_things_index_path
     else
       render @good_thing
     end
   end
 
+def update
+  @good_thing = GoodThing.find_by(
+    start_date:good_thing_params[:start_date],user_id:current_user.id
+  ).update(good_thing_params)
+
+  redirect_to good_things_index_path
+
+end
+
   private
   
   def good_thing_params
-    params.require(:good_thing).permit(:good_1, :good_2, :good_3,:start_date).merge(user_id: current_user.id)
+    params.require(:good_thing
+    ).permit(:good_1, :good_2, :good_3,:start_date
+    ).merge(user_id: current_user.id)
   end
 
 
