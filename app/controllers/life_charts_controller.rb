417eb0a427bdde65763bc87bf5e9eb7b89bdead0
nagacchiken <vx458pl4@gmail.com>
2020-10-19 21:27:21 +0900

    ゲストログイン機能の修正

--- app/controllers/life_charts_controller.rb
+++ app/controllers/life_charts_controller.rb
@@ -7,6 +7,7 @@ class LifeChartsController < ApplicationController
     user = User.find_or_create_by!(email: 'guest@example.com') do |user|
       user.nickname = "Guest"
       user.password = SecureRandom.urlsafe_base64
+      user.password_confirmation = user.password
     end
     sign_in user
     redirect_to root_path
