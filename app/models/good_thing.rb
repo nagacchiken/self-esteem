a1b814a17d76b280369afd0478ad1a05ad6944aa
nagacchiken <vx458pl4@gmail.com>
2020-10-26 16:47:08 +0900

    バリデーションの実装

--- app/models/good_thing.rb
+++ app/models/good_thing.rb
@@ -1,5 +1,9 @@
 class GoodThing < ApplicationRecord
   belongs_to :user
-
-  validates :start_date, presence: true
+  with_options presence: true do
+   validates :start_date, uniqueness: true
+   validates :good_1
+   validates :good_2
+   validates :good_3
+  end
 end
