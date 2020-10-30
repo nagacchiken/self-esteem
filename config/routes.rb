1cf58c62ca2f8ec8913928d6798198c18f251f17
nagacchiken <vx458pl4@gmail.com>
2020-10-30 16:33:09 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get 'good_things/index'
   devise_for :users
   root to: 'life_charts#index'
-  resource :life_charts, only: [:index]
   post '/life_charts/guest_sign_in', to: 'life_charts#new_guest'
   resources :good_things, only: [:index,:create,:update,:show,:new]
 
