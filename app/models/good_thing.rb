41208e7b01c22bf674ac7be164705fe5a2bf35d8
nagacchiken <vx458pl4@gmail.com>
2020-12-13 14:34:25 +0900

    同じ日付に投稿できないよう実装

--- app/models/good_thing.rb
+++ app/models/good_thing.rb
@@ -1,7 +1,7 @@
 class GoodThing < ApplicationRecord
   belongs_to :user
   with_options presence: true do
-    validates :start_date
+    validates :start_date, uniqueness: true
     validates :good_1
     validates :good_2
     validates :good_3
